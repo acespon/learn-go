c9d7c1713874c186352b4b533489b7756584b898
Arthur Cespon <arthur.cespon@dailypay.com>
2024-06-11 13:43:05 +0100

    feat: Arrays and Slices

--- 3-Iteration/repeat_test.go
+++ 3-Iteration/repeat_test.go
@@ -3,16 +3,17 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
