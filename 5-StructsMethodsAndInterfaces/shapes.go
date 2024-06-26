58103ff034ee3c8af6d5f7193e719f4cd94ad5fb
Arthur Cespon <arthur.cespon@dailypay.com>
2024-06-26 14:15:02 +0100

    Structs refactor

--- 5-StructsMethodsAndInterfaces/shapes.go
+++ 5-StructsMethodsAndInterfaces/shapes.go
@@ -6,6 +6,15 @@ type Shape interface {
 	Area() float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
